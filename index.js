bdfc24d2df39409db4b713968d4e2ad2e0fc0be9
barangievans <barangievans@gmail.com>
2024-07-07 01:54:26 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); // Output: 62
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+let numbers = [10, 5, 20, 15]; 
+let max = Math.max(...numbers);
+expect(max).to.equal(20);
